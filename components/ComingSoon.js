c8e25e339fbf6b1dc75905771d75354c12996217
Neo945 <shreeshsrvstv@gmail.com>
2023-05-30 18:16:45 +0530

    :tada: feat: Add error message page

---
+++ components/ComingSoon.js
@@ -0,0 +1,23 @@
+export default function ComingSoon() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <div style={{ display: "flex" }}>
+        <div
+          style={{
+            width: "1px",
+            margin: "0 20px",
+            backgroundColor: "white",
+          }}
+        ></div>
+        <h1>Opps!! Website coming soon in your size</h1>
+      </div>
+    </div>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,26 @@
 import Head from "next/head";
 import "../styles/globals.css";
+import { useEffect, useState } from "react";
+import ComingSoon from "../components/ComingSoon";
 
 function MyApp({ Component, pageProps }) {
+  const [isWindowSize, setIsWindowSize] = useState(true);
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      if (window.innerWidth < 896) {
+        setIsWindowSize(false);
+      } else {
+        setIsWindowSize(true);
+      }
+    });
+  }, []);
   return (
     <>
       <Head>
         <link rel="icon" href="/vercel.svg" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Component {...pageProps} />
+      {isWindowSize ? <Component {...pageProps} /> : <ComingSoon />}
     </>
   );
 }

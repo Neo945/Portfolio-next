8a23507d009099d75e04c1b6e7055baa974cfe88
Neo945 <shreeshsrvstv@gmail.com>
2023-06-25 00:13:28 +0530

    Add loader

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,19 @@
 import Head from "next/head";
 import "../styles/globals.css";
 import { useEffect, useState } from "react";
-import ComingSoon from "../components/ComingSoon/ComingSoon";
 
 function MyApp({ Component, pageProps }) {
   const [isWindowSize, setIsWindowSize] = useState(false);
   useEffect(() => {
-    const handleSize = () => {
-      // console.log(window.innerWidth);
-      if (window.innerWidth < 896) {
-        setIsWindowSize(false);
-      } else {
-        setIsWindowSize(true);
-      }
-    };
-    window.addEventListener("load", handleSize);
-    window.addEventListener("resize", handleSize);
+    const loader = document.getElementById("globalLoader");
+    if (loader) loader.style.display = "none";
+    if (window.innerWidth < 896) {
+      setIsWindowSize(false);
+    } else {
+      const opps = document.getElementById("opps");
+      if (opps) opps.style.display = "none";
+      setIsWindowSize(true);
+    }
   }, []);
   return (
     <>
@@ -24,7 +22,6 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} isWindowSize={isWindowSize} />
-      {/* {isWindowSize ? null : <ComingSoon />} */}
     </>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,89 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+const loaderStyle = `
+body{
+display: block;
+}
+#oppsbg {
+  width: 1px;
+  margin: 0 20px;
+  background-color: white;
+}
+#opps {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  background-color: white;
+  z-index: 1600;
+}
+#flex {
+  display: flex;
+}
+#globalLoader{
+    position: fixed;
+    z-index: 1700;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    display: flex;
+    left: 0,
+    right: 0;
+    width: 100%;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+}
+
+.loader {
+ --clr: #3498db;
+ /* color of spining  */
+ width: 50px;
+ height: 50px;
+ position: relative;
+}
+
+.loader div:nth-child(1), .loader div:nth-child(2) {
+ content: "";
+ position: absolute;
+ top: -10px;
+ left: -10px;
+ width: 100%;
+ height: 100%;
+ border-radius: 100%;
+ border: 10px solid transparent;
+ border-top-color: var(--clr);
+}
+
+.loader div:nth-child(1) {
+ z-index: 100;
+ animation: spin 1s infinite;
+}
+
+.loader div:nth-child(2) {
+ border: 10px solid #ccc;
+}
+
+@keyframes spin {
+ 0% {
+  -webkit-transform: rotate(0deg);
+  -ms-transform: rotate(0deg);
+  -o-transform: rotate(0deg);
+  transform: rotate(0deg);
+ }
+ 100% {
+  -webkit-transform: rotate(360deg);
+  -ms-transform: rotate(360deg);
+  -o-transform: rotate(360deg);
+  transform: rotate(360deg);
+ }
+}`;
+
 export default function Document() {
   return (
     <Html>
@@ -11,7 +95,25 @@ export default function Document() {
           rel="stylesheet"
         />
       </Head>
+      <head>
+        <style>{loaderStyle}</style>
+      </head>
       <body>
+        {/* loader */}
+        <div id={"globalLoader"}>
+          <div className="loader">
+            <div />
+            <div />
+          </div>
+        </div>
+
+        {/* opps */}
+        <div id={"opps"}>
+          <div id="flex">
+            <div id="oppsbg"></div>
+            <h1>Opps!! Website coming soon in your size</h1>
+          </div>
+        </div>
         <Main />
         <NextScript />
       </body>
